2973b003973ce87c8d09d963c0f3b5c6ba6a309f
dmitry.oleinik <dmitry.oleinik@rhinocodes.com>
2022-12-25 06:29:35 +0200

    change ip serverr

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -31,7 +31,7 @@ const ProductList = () => {
             totalPrice: getTotalPrice(addedItems),
             queryId,
         }
-        fetch('http://165.22.202.4:8000/web-data', {
+        fetch('http://174.138.105.203:8000/web-data', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
